01d5e006824f825644f61a28d9fa21603150405d
Michal Charemza <michalcharemza@gmail.com>
2010-05-09 18:27:26 +0100

    Added code and swf

---
+++ Source/FullScreenImage.js
@@ -0,0 +1,53 @@
+var FullScreenImage = new Class({
+	
+	Implements: [Options, Events],
+	
+	options: {
+		swf: 'FullScreenImage.swf'
+	},
+	
+	initialize: function(button, options) {
+		this.setOptions(options);
+		this.button = button;
+		this.loaded = false;
+		if (Browser.Plugins.Flash.version >= 9) this.load();
+	},
+	
+	load: function() {
+		this.fireEvent('loading');
+		var coords = this.button.getCoordinates();
+		
+		this.swf = new Swiff(this.options.swf, {
+			height: coords.height,
+			width: coords.width,
+			params: {
+				allowFullScreen: true
+			},
+			callBacks: {
+				onSWFLoad: this.onSWFLoad.bind(this)
+			},
+			style: 'position: absolute'
+		});
+		this.button.setStyle('position', 'relative');
+		var wrapper = new Element('div', {
+			styles: {
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				height: coords.height,
+				width: coords.width
+			},
+		});
+		this.button.grab(wrapper.grab(this.swf));
+	},
+	
+	onSWFLoad: function() {
+		this.loaded = true;
+		this.fireEvent('load');
+	},
+	
+	show: function(src) {
+		if (this.loaded) this.swf.remote("show", src);
+	}
+	
+});
